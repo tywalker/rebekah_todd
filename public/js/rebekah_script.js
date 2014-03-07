36332d5430664838f23aec0cd6902054e943c8e3
Ty Walker <walkerje1988@yahoo.com>
2014-03-07 18:18:37 -0500

    cleaned code, jquery min, fixed tree, finalization process

--- public/js/rebekah_script.js
+++ public/js/rebekah_script.js
@@ -1,98 +1,98 @@
 $("document").ready(function() {
-	
-	$(window).load(function(){
-	  $('#loader').fadeOut(3000);
-	});
 
-	$(".player, #about_rebekah_fixed").hide();
+	function init() {
+		var scrollTop;
+		var imgPos;
+		var xPos;
 
-	$(".listen").click(function(event) {
-		event.preventDefault();
-		$(".player").toggle(200);
-	});
-	$('body').click(function() {
-		alert($(window).scrollTop());
-		alert($(window).width());
-		alert(($(window).width()/2) - ($(".slider img:first").width()/2));
-	});
+		// sets loading gif until images are loaded
+		$(window).load(function(){
+		  $('#loader').fadeOut(3000);
+		}); // end load
 
-	$(window).resize(function() {
-		var scrollTop = $(window).scrollTop();
-		var imgPos = -(($(window).width()/2) - ($(".slider img:first").width()/2));
-		var xPos = ((scrollTop - 7560) - imgPos);
-		$(".slider").css({'position' : 'relative', 'right' : imgPos + 'px'});
-		if (scrollTop >= 2300) {
-			$(".slider").css({'position' : 'fixed'});
-			$(".slider").css({
-				'right': xPos +'px'
-			});
-		}		
-	});
+		// hide divs that will be faded in and out later
+		$(".player, #about_rebekah_fixed").hide();
+
+		// shows music player
+		$(".listen").click(function(event) {
+			event.preventDefault();
+			$(".player").toggle(200);
+		}); // end click
+
+		// resizes gallery when browser is adjusted
+		$(function() {
+			$(window).resize(function() {
+				scrollTop = $(window).scrollTop();
+				imgPos = -(($(window).width()/2) - ($(".slider img:first").width()/2));
+				xPos = ((scrollTop - 7560) - imgPos);
+
+				// centers first gallery img, consequently making sure gallery scrolls smoothly through
+				// the process of sizing and resizing
+				$(".slider").css({'position' : 'relative', 'right' : imgPos + 'px'});
+				
+				// adjusts gallery if gallery scroll has begun
+				if (scrollTop >= 2300) {
+					$(".slider").css({'position' : 'fixed'});
+					$(".slider").css({
+						'right': xPos +'px'
+					});
+				}		
+			}); // end window resize
+		}); // end function
+
+		// activates animation for anchor nav tags
+	    $(function() {
+		    $('a[href*=#]:not([href=#])').click(function() {
+		      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		        if (target.length) {
+		          $('html,body').animate({
+		            scrollTop: target.offset().top
+		          }, 1500);
+		          return false;
+		        }
+		      }
+		    }); // end click
+		}); // end function
+
+	    // function for gallery inititiation
+	    $(function() {
+			$(window).scroll(function() {
+				scrollTop = $(window).scrollTop();
+				imgPos = -(($(window).width()/2) - ($(".slider img:first").width()/2));
+				xPos = ((scrollTop - 7560) - imgPos);
+
+				// fades about info in and out, before and after #about div
+				if (scrollTop >= 2250 && scrollTop <= 6700) {
+					$("#about_rebekah_fixed").fadeIn(500);
+				}
+				else {
+					$("#about_rebekah_fixed").fadeOut(500);
+				}
+
+				// switches position to fixed, and then slides gallery left minus the img center
+				// and minus the scrolltop position in which the gallery scroll has begun.
+				if (scrollTop >= 2300 && scrollTop <= 7100) {
+					$(".slider").css({'position' : 'fixed'});
+					$(".slider").fadeIn(500);
+					$(".slider").css({
+						'right': xPos +'px'
+					});
+				}
+				// sets gallery to relative so visible before scroll
+				else if(scrollTop <= 2300) {
+					$(".slider").css({'position' : 'relative', 'right' : imgPos + 'px'});
+				}
+				// fades gallery after scrolling past
+				else {
+					$(".slider").fadeOut(500);
+				}
+			}); // end scroll
+		}); // end function
+	} // end init
 
 
-    $(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top
-	        }, 1500);
-	        return false;
-	      }
-	    }
-	  });
-	});
-
-	$(window).scroll(function() {
-		var scrollTop = $(window).scrollTop();
-		var imgPos = -(($(window).width()/2) - ($(".slider img:first").width()/2));
-		var xPos = ((scrollTop - 7560) - imgPos);
-
-		if (scrollTop >= 2250 && scrollTop <= 6700) {
-			$("#about_rebekah_fixed").fadeIn(500);
-		}
-		else {
-			$("#about_rebekah_fixed").fadeOut(500);
-		}
-
-		if (scrollTop >= 2300 && scrollTop <= 7100) {
-			$(".slider").css({'position' : 'fixed'});
-			$(".slider").fadeIn(500);
-			$(".slider").css({
-				'right': xPos +'px'
-			});
-		}
-		else if(scrollTop <= 2300) {
-			$(".slider").css({'position' : 'relative', 'right' : imgPos + 'px'});
-		}
-		else {
-			$(".slider").fadeOut(500);
-		}
-	});
-
-
-
-
-
-
-	$(window).scroll(function() {
-		var yPos = $(window).scrollTop(),
-		imgHeight = yPos - 8100;
-			if (yPos > 8100 && yPos < 8650) {
-				$(".roots_center, .roots_side").show();
-				$(".roots_center").css({
-					height: imgHeight + "px"
-				}); // end css
-				$(".roots_side").animate({
-					width: imgHeight + "px"
-				}, 100); //end animate
-
-			}// end if statement
-			else if(yPos<8100) {
-				$(".roots_center, .roots_side").hide()
-			}
-
-	}); // end scroll
+	// let's get this party started!
+	init();
 }); // end document ready
